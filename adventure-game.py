38d6b7bd4161d35d1c4ac07ec3494cfd5219375c
Smekiboy <64141156+Smekiboy@users.noreply.github.com>
2020-08-07 14:41:11 +0200

    Update adventure-game.py

--- adventure-game.py
+++ adventure-game.py
@@ -1,3 +1,4 @@
+
 import time
 import random
 
@@ -17,37 +18,35 @@ def intro(item, alternativ):
 
 def house(item, alternativ):
     print_pause("You approach the door of the house.")
-    print_pause("You are about to knock when the door opens and out steps a" + alternativ + ".")
-    print_pause("Eep! This is the" + alternativ + "'s house!")
-    print_pause("The" + alternativ + "attacks you!")
+    print_pause("You are about to knock when the door opens and out steps a " + alternativ + ".")
+    print_pause("Eep! This is the " + alternativ + "'s house!")
+    print_pause("The " + alternativ + " attacks you!")
     if "spear" not in item:
         print_pause("You feel a bit under-prepared for this, "
                      "what with only having a tiny dagger.")
     fight(item, alternativ)
         
 def fight(item, alternativ):
-    while True:
-        response = input("Would you like to (1) fight or (2) run away?")
-        if "1" i == response:
-            if "spear" in item:
-                print_pause("As the" + alternativ + " moves to attack, "
-                            "you unsheath your new spear.")
-                print_pause("The Spear of Ogoroth shines brightly in "
-                            "your hand as you brace yourself for the attack.")
-                print_pause("But the" + alternativ + "takes one "
-                            "look at your shiny new toy and runs away!")
-                print_pause("You have rid the town of the" + alternativ + ". " 
-                            "You are victorious!")
-            else:
-                print_pause("You do your best...")
-                print_pause("But your dagger is no match for the" + alternativ + ".")
-                print_pause("You have been defeated!")
-            play_again()
-        elif "2" == response:
-            print_pause("You run back into the field. Luckily, " 
+    response = input("Would you like to (1) fight or (2) run away?")
+    if "1" == response:
+        if "spear" in item:
+            print_pause("As the " + alternativ + " moves to attack, "
+                        "you unsheath your new spear.")
+            print_pause("The Spear of Ogoroth shines brightly in "
+                        "your hand as you brace yourself for the attack.")
+            print_pause("But the " + alternativ + "takes one "
+                        "look at your shiny new toy and runs away!") 
+            print_pause("You have rid the town of the " + alternativ + ". " 
+                        "You are victorious!")
+        else:
+            print_pause("You do your best...")
+            print_pause("But your dagger is no match for the " + alternativ + ".")
+            print_pause("You have been defeated!")
+        play_again()
+    elif "2" == response:
+        print_pause("You run back into the field. Luckily, " 
                         "you don't seem to have been followed.")
-            field(item, alternativ)
-            
+        field(item, alternativ)
 
 def cave(item, alternativ):
     if "spear" in item:
@@ -74,35 +73,32 @@ def field(item, alternativ):
     print_pause("Enter 2 to peer into the cave.")
     print_pause("What would you like to do?")
 
-    while True:
-        response = input("(Please enter 1 or 2).\n")
-        if "1" in response:
-            print_pause("You are in the house!")
-            house(item, alternativ)
-            break
-        elif "2" in response:
-            print_pause("You are in the cave!")
-            cave(item, alternativ)
-            break
-        else:
-            print_pause("I don't understand!")
+    response = input("(Please enter 1 or 2).\n")
+    if "1" in response:
+        print_pause("You are in the house!")
+        house(item, alternativ)
+    elif "2" in response:
+        print_pause("You are in the cave!")
+        cave(item, alternativ)
+    else:
+        print_pause("I don't understand!, Try again")
+        field(item, alternativ)
 
 def play_again():
-    option = input("Would you like to play again? "
-                    "Please say 'yes' or 'no'.\n",
-                    "yes", "no").lower()
-    if "yes" in response:
+    option = input("Would you like to play again? \n"+
+                    "Please say 'yes' or 'no'.\n").lower()
+    if "yes" == option:
         print_pause("Great! We will restart the game\n")
         play_game()
-    elif "no" in response:
+    elif "no" == option:
         print_pause("Thank you for playng. You are welcome again!\n")
     else:
-        play_game()
-
+        print("Unable to undestand")
+        play_again()
 
 def play_game():
     item = []
-    alternativ = random.choice(["pirate", "gorgon", "dragon", "troll" "wicked fairie"])
+    alternativ = random.choice(["pirate", "gorgon", "dragon", "troll", "wicked fairie"])
     intro(item, alternativ)
     field(item, alternativ)
 play_game()
